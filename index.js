2f6598e6ee83a9b429ef04796420ca5923b7c8c9
NoorHajDawood <noorhajdawood@gmail.com>
2021-11-13 14:52:32 +0200

    creation of express base

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-const http = require ("http");
-const port = 8080;
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8080;
 
-http.createServer((req, res) => {
-	res.writeHead(200); // Status code in header
-	res.write("shoval"); // Send res body (can write multiple times)
-	res.end(); // Send & close connection
-}).listen(port); // Listen for connection on this port
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-console.log(`Listening on port ${port}`);
+app.get('/api/v1/githubUser/:githubUserName/avatar', (req, res) => {
+	res.send(`avatar page of github user ${req.params.githubUserName}`)
+}).get('/api/v1/githubUser/:githubUserName/repo/:repoName', (req, res) => {
+	res.send(`Repo: ${req.params.repoName} of user ${req.params.githubUserName}`)
+}).get('/api/v1/githubUser/:githubUserName/repo/:repoName/contributers', (req, res) => {
+	res.send(`Contributers page of repo ${req.params.repoName} of user ${req.params.githubUserName}`)
+}).all('*', (req, res) => {
+	res.send('Global handler for all routes');
+}).listen(port, () => {
+	console.log(`Listening on port ${port}`);
+});
+
